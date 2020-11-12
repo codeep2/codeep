2fc9b1b55940d4933237ee04e960416328922d99
codeep2 <codeep37@gmail.com>
2020-11-12 15:28:22 +0800

    blog基础框架完成

---
+++ components/TheFooter.vue
@@ -0,0 +1,82 @@
+<template>
+  <footer>
+    <div class="copyright">
+      <p>Copyright ©{{ new Date().getFullYear() }} | Codeep's blog</p>
+    </div>
+    <div class="description">
+      <p>
+        Powered by
+        <span>
+          <ul>
+            <li
+              v-for="tech in technology"
+              :key="tech"
+              :style="{color: tech.color}"
+            >
+              {{ tech.content }}
+            </li>
+          </ul>
+        </span> on
+        <a href="https://github.com/codeep2">
+          GitHub
+        </a> ·
+        Designed by
+        <nuxt-link to="/">
+          Codeep
+        </nuxt-link>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      technology: [
+        { content: 'HTML5', color: '#e96228' },
+        { content: 'JS', color: '#2f495e' },
+        { content: 'Sass', color: '#c76494' },
+        { content: 'Vuejs', color: '#41b883' },
+        { content: 'Nuxtjs', color: '#108775' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @keyframes loop {
+    0% {transform: translateY(0);}
+    20% {transform: translateY(-20%);}
+    40% {transform: translateY(-40%);}
+    60% {transform: translateY(-60%);}
+    80% {transform: translateY(-80%);}
+    100% {transform: translateY(0%);}
+  }
+  footer {
+    text-align: center;
+    a {
+      color: black;
+      text-decoration: underline;
+      &:hover {
+        background-color: #dedede;
+        opacity: 0.6;
+        transition: opacity 3s linear;
+      }
+    }
+    .description {
+      span {
+        display: inline-block;
+        height: 21px;
+        margin-bottom: -3px;
+        overflow: hidden;
+        ul {
+          display: inline-block;
+          transform: translateY(0);
+          animation: loop 12s linear infinite alternate;
+        }
+      }
+    }
+  }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="page">
+    <TheHeader />
+    <Nuxt />
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader.vue'
+import TheFooter from '@/components/TheFooter.vue'
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  }
+}
+</script>
+
+<style>
+  .page {
+    margin: 0 auto;
+    max-width: 960px;
+  }
+</style>
