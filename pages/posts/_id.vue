5c57773ba5ba5d30b90bed6fa9a7fe14a9a958d0
codeep2 <codeep37@gmail.com>
2020-11-17 18:19:36 +0800

    style: 优化文章详情页面样式

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -22,6 +22,7 @@ export default {
     padding: 10px 20px;
     > h1 {
       font-size: 36px;
+      text-align: center;
       margin-bottom: 40px;
     }
     article {
@@ -29,20 +30,40 @@ export default {
       h2, h3,
       h4, h5, h6 {
         font-weight: bold;
+        margin-bottom: 15px;
       }
-      h1 { font-size: 28px; }
-      h2 { font-size: 25px; }
-      h3 { font-size: 22px; }
-      h4 { font-size: 19px; }
-      h5 { font-size: 16px; }
-      h6 { font-size: 13px; }
+      h1 {
+        color: #2196f3;
+        font-size: 28px;
+      }
+      h2 {
+        padding-bottom: 6px;
+        color: #03a9f4;
+        font-size: 23px;
+        border-bottom: 1px solid #eaecef;
+      }
+      h3 {
+        color: #3ab8f1;
+        font-size: 18px;
+      }
+      h4 {
+        color: #4ec0f5;
+        font-size: 13px;
+      }
+      p,
+      li {
+        font-family: 'Source Han Serif', 'MicroSoft Yahei', Georgia, serif;;
+      }
       p {
         margin-bottom: 24px;
       }
       img {
         display: block;
         margin: 15px auto;
-        max-width: 94%;
+        max-width: 92%;
+      }
+      li > pre {
+        margin-top: 10px;
       }
       ul {
         margin: 18px 0;
@@ -59,6 +80,7 @@ export default {
       }
       ol {
         margin-top: 20px;
+        margin-bottom: 8px;
         padding-left: 35px;
         li {
           list-style: decimal;
@@ -71,12 +93,15 @@ export default {
       }
       pre {
         margin: 24px 0;
-        padding: 10px;
-        color: #dcdcdc;;
-        font-size: 14px;
+        padding: 8px;
         border-radius: 10px;
         background-color: #1e1e1e;
-        opacity: 0.99;
+        code {
+          color: #f8f8f2;
+          font-size: 14px;
+          line-height: 1.3;
+          font-family: Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace;
+        }
       }
       blockquote {
         background-color: #e4e4e47a;
@@ -100,7 +125,16 @@ export default {
         }
         &:before, &:after {
           font-size: 36px;
-          color: #ccc;
+          color: #ffa7a7;
+        }
+      }
+      a {
+        color: #f18c35;
+        text-decoration: underline;
+        &:hover {
+          background-color: #dedede;
+          opacity: 0.6;
+          transition: opacity 3s linear;
         }
       }
     }
